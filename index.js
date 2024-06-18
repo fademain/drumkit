8a1c69782d29ecf9b0b8d92dd263a8268d2289b5
Bru <64201997+fademain@users.noreply.github.com>
2024-06-18 22:36:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+//for (var i = 0, i<document.querySelectorAll(".drum").length); {
+
+//document.querySelectorAll("drum")[i].addEventListener("click", handleClick);
+
+//function handleClick() {alert("I got clicked!"); }
+
+// <script src="index.js" charset="utf-8"></script>
